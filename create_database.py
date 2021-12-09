17fb05e26e30f79f20f0ed83586ef67cb863cb55
21zz <zanmanfan11@gmail.com>
2021-12-09 03:10:22 -0500

    ez re-create database python script

---
+++ create_database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+"""
+Recreates database.
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@ WILL DELETE ALL DATA @@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ONLY USE TO COMPLETELY REGENERATE DATABASE.
+"""
+# https://stackoverflow.com/questions/54289555/how-do-i-execute-an-sqlite-script-from-within-python
+with open('database.sql', 'r') as sql_file:
+    sql_script = sql_file.read()
+
+
+db = sqlite3.connect('fisherbot.db')
+cursor = db.cursor()
+cursor.executescript(sql_script)
+db.commit()
+print("Done.")
+db.close()
