d4c724dbc17c18f675047a4428d8e9f9f7b16429
21zz <zanmanfan11@gmail.com>
2022-10-07 00:57:47 -0400

    move some stuff around

--- util/dbf.py
+++ util/dbf.py
@@ -10,7 +10,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-
 import sqlite3
 from random import Random
 import os
